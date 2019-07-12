4a78e5f12cf6db121ef59ce13bbf056ffed893e4
Kateryna Storozh <storozhkateryna@gmail.com>
2019-07-12 14:48:55 +0300

    make retrofit static

--- data/src/main/java/com/kstorozh/data/utils/MyRetrofit.kt
+++ data/src/main/java/com/kstorozh/data/utils/MyRetrofit.kt
@@ -20,7 +20,7 @@ internal class MyRetrofit {
                     retrofit = Retrofit.Builder()
                         .addConverterFactory(GsonConverterFactory.create())
                         .baseUrl(BASE_URL)
-                        .client(okHttpClient)
+                        .client(okHttpClient!!)
                         .build()
                 }
             }
