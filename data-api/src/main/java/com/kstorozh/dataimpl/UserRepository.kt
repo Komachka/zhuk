c742e6c993c1ccaa6acaa641a98032f9064a2ff0
Komachka <katuxaz@ukr.net>
2019-07-30 23:03:31 +0300

    rewrite user repo

--- data-api/src/main/java/com/kstorozh/dataimpl/UserRepository.kt
+++ data-api/src/main/java/com/kstorozh/dataimpl/UserRepository.kt
@@ -4,11 +4,12 @@ import androidx.lifecycle.MutableLiveData
 import com.kstorozh.dataimpl.model.UserLoginParam
 
 import com.kstorozh.dataimpl.DataError
+import com.kstorozh.dataimpl.model.out.RepoResult
 import com.kstorozh.dataimpl.model.out.SlackUser
 
 interface UserRepository {
-    suspend fun getUsers(): List<SlackUser>
-    suspend fun remindPin(slackUserId: String): Boolean
+    suspend fun getUsers(): RepoResult<List<SlackUser>>
+    suspend fun remindPin(slackUserId: String): RepoResult<Boolean>
     suspend fun getErrors(): MutableLiveData<DataError>
-    suspend fun login(userLoginParam: UserLoginParam): String?
+    suspend fun login(userLoginParam: UserLoginParam): RepoResult<String>
 }
--- data/src/main/java/com/kstorozh/data/repository/UserRepositoryImpl.kt
+++ data/src/main/java/com/kstorozh/data/repository/UserRepositoryImpl.kt
@@ -9,27 +9,30 @@ import com.kstorozh.data.network.RemoteData
 import com.kstorozh.data.utils.createError
 import com.kstorozh.dataimpl.DataError
 import com.kstorozh.dataimpl.model.UserLoginParam
+import com.kstorozh.dataimpl.model.out.BookingSessionData
+import com.kstorozh.dataimpl.model.out.RepoResult
 import com.kstorozh.dataimpl.model.out.SlackUser
 
 internal class UserRepositoryImpl(
     private val remoteData: RemoteData,
-    private val mapper: UserDataMapper,
-    private val users: ArrayList<SlackUser>
+    private val mapper: UserDataMapper
+    //private val users: ArrayList<SlackUser>
 ) : UserRepository {
 
     private val myError: MutableLiveData<DataError> = MutableLiveData()
 
-    override suspend fun login(userLoginParam: UserLoginParam): String? {
-        Log.d(LOG_TAG, userLoginParam.toString())
+    override suspend fun login(userLoginParam: UserLoginParam): RepoResult<String> {
+        val repoResult: RepoResult<String> = RepoResult()
         return when (val result = remoteData.login(mapper.mapUserLoginParam(userLoginParam))) {
             is ApiResult.Success -> {
-                result.data.data.userId.toString()
+                repoResult.data = result.data.data.userId.toString()
+                repoResult
             }
             is ApiResult.Error<*> -> {
-
                 myError.postValue(createError(Endpoints.LOGIN, result))
                 Log.d(LOG_TAG, "My error $myError")
-                null
+                repoResult.error = createError(Endpoints.LOGIN, result)
+                repoResult
             }
         }
     }
@@ -37,27 +40,34 @@ internal class UserRepositoryImpl(
     override suspend fun getErrors(): MutableLiveData<DataError> {
         return myError
     }
-    override suspend fun getUsers(): List<SlackUser> {
+    override suspend fun getUsers(): RepoResult<List<SlackUser>> {
+        val repoResult: RepoResult<List<SlackUser>> = RepoResult()
+        val users: ArrayList<SlackUser> = ArrayList()
         when (val result = remoteData.getUsers()) {
             is ApiResult.Success -> {
                 users.addAll(mapper.mapSlackUserList(result.data.users))
+                repoResult.data = users
             }
             is ApiResult.Error<*> -> {
                 myError.postValue(createError(Endpoints.GET_USERS, result))
+                repoResult.error = createError(Endpoints.GET_USERS, result)
             }
         }
-        return users
+        return repoResult
     }
 
-    override suspend fun remindPin(slackUserId: String): Boolean {
-
+    override suspend fun remindPin(slackUserId: String): RepoResult<Boolean> {
+        val repoResult: RepoResult<Boolean> = RepoResult()
         return when (val result = remoteData.remindPin(slackUserId)) {
             is ApiResult.Success -> {
-                true
+                repoResult.data = true
+                repoResult
             }
             is ApiResult.Error<*> -> {
                 myError.postValue(createError(Endpoints.REMIND_PIN, result))
-                false
+                repoResult.data = false
+                repoResult.error = createError(Endpoints.REMIND_PIN, result)
+                repoResult
             }
         }
     }
