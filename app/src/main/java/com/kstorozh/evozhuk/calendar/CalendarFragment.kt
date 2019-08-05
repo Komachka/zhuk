da651d3808017935b24071604bf3f5507b4c7211
Kateryna Storozh <storozhkateryna@gmail.com>
2019-08-05 18:20:22 +0300

    [https://gitlab.com/evo.company/evo.zhuk-android/issues/2] change date format for snackbar

--- app/src/main/java/com/kstorozh/evozhuk/calendar/CalendarFragment.kt
+++ app/src/main/java/com/kstorozh/evozhuk/calendar/CalendarFragment.kt
@@ -5,15 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.kstorozh.evozhuk.DATE_FORMAT_DAY
 import com.kstorozh.evozhuk.R
 
-import com.applandeo.materialcalendarview.EventDay
 import kotlinx.android.synthetic.main.fragment_calendar.view.*
-import android.widget.Toast
 import com.kstorozh.evozhuk.DATE_FORMAT_NOTIFICATION_MESSAGE
 import com.kstorozh.evozhuk.utils.showSnackbar
 import java.text.SimpleDateFormat
-import java.util.logging.SimpleFormatter
 
 class CalendarFragment : Fragment() {
 
@@ -28,10 +26,8 @@ class CalendarFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         view.calendarView.setOnDayClickListener { eventDay ->
             view.showSnackbar(
-                SimpleDateFormat(DATE_FORMAT_NOTIFICATION_MESSAGE).format(eventDay.calendar.timeInMillis)
+                SimpleDateFormat(DATE_FORMAT_DAY).format(eventDay.calendar.timeInMillis)
             )
         }
     }
-
-
 }
