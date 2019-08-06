6c5b283751547b6be5e6d598c2a65e6dda4ca669
Kateryna Storozh <storozhkateryna@gmail.com>
2019-08-06 14:27:47 +0300

    [https://gitlab.com/evo.company/evo.zhuk-android/issues/8] show snackbar if battary low

--- app/src/main/java/com/kstorozh/evozhuk/returnDevice/BatteryLevelCheck.kt
+++ app/src/main/java/com/kstorozh/evozhuk/returnDevice/BatteryLevelCheck.kt
@@ -4,18 +4,16 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.BatteryManager
 import android.view.View
-import android.widget.TextView
 import com.kstorozh.evozhuk.BATTERY_LEVEL_TO_CHARGE
-import com.kstorozh.evozhuk.R
+import kotlinx.android.synthetic.main.fragment_return_device.view.*
 
 interface BatteryLevelCheck {
 
     fun View.manageBatteryCharge() {
-        val needToCharge = findViewById<TextView>(R.id.needToChargeTv)
         val ifilter = IntentFilter(Intent.ACTION_BATTERY_CHANGED)
         val batteryStatus = context?.registerReceiver(null, ifilter)
         val level = batteryStatus?.getIntExtra(BatteryManager.EXTRA_LEVEL, -1)
         if (level!! < BATTERY_LEVEL_TO_CHARGE)
-            needToCharge.text = "${resources.getString(R.string.chargeDeviceMessage)}"
+            LowBatterySnackbar.make(this.snackbarlocation)?.show()
     }
 }
