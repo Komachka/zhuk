a1c731f2ca31da513b56103ca94cafd9a4617a1b
Kateryna Storozh <storozhkateryna@gmail.com>
2019-08-06 11:48:04 +0300

    [https://gitlab.com/evo.company/evo.zhuk-android/issues/6] add not null context check

--- app/src/main/java/com/kstorozh/evozhuk/login/UserNamesDataHandler.kt
+++ app/src/main/java/com/kstorozh/evozhuk/login/UserNamesDataHandler.kt
@@ -1,16 +1,18 @@
 package com.kstorozh.evozhuk.login
 
-import android.R
 import android.widget.ArrayAdapter
+import android.R
 import com.kstorozh.evozhuk.utils.observe
 import kotlinx.android.synthetic.main.fragment_login.*
 
 interface UserNamesDataHandler {
 
     fun LoginFragment.subscribeNamesLiveData() {
-        observe(model.getUserNames()) {
-            userNames = it
-            this.loginEt.setAdapter(ArrayAdapter(context!!, R.layout.simple_dropdown_item_1line, it.toTypedArray()))
+        observe(model.getUserNames()) { list ->
+            userNames = list
+            context?.let {
+                this.loginEt.setAdapter(ArrayAdapter(it, R.layout.simple_dropdown_item_1line, list.toTypedArray()))
+            }
         }
     }
 }
