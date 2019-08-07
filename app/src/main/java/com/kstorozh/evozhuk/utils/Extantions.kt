45034bd3a0814a335a2e63b3604a04fb737e5dd4
Kateryna Storozh <storozhkateryna@gmail.com>
2019-08-07 14:43:55 +0300

    [https://gitlab.com/evo.company/evo.zhuk-android/issues/8] remove comments

--- app/src/main/java/com/kstorozh/evozhuk/utils/Extantions.kt
+++ app/src/main/java/com/kstorozh/evozhuk/utils/Extantions.kt
@@ -126,30 +126,23 @@ private fun Long.mgToGb() = this * 0.001
 private fun Long.biteToMg() = this / 0X100000 // 1024 * 1024
 private fun Long.biteToGb() = biteToMg().mgToGb()
 
-/**
- * copy findSuitableParent from Snackbar class
- * https://github.com/material-components/material-components-android/blob/1.0.0/lib/java/com/google/android/material/snackbar/Snackbar.java
- */
-
 fun View?.findSuitableParent(): ViewGroup? {
     var view = this
     var fallback: ViewGroup? = null
     do {
-        if (view is CoordinatorLayout) { // We've found a CoordinatorLayout, use it
+        if (view is CoordinatorLayout) {
             return view
         } else if (view is FrameLayout) {
-            if (view.id == android.R.id.content) { // If we've hit the decor content view, then we didn't find a CoL in the hierarchy, so use it.
+            if (view.id == android.R.id.content) {
                 return view
             } else {
-                fallback = view // It's not the content view but we'll use it as our fallback
+                fallback = view
             }
         }
         if (view != null) {
-            // Else, we will loop and crawl up the view hierarchy and try to find a parent
             val parent = view.parent
             view = if (parent is View) parent else null
         }
     } while (view != null)
-    // If we reach here then we didn't find a CoL or a suitable content view so we'll fallback
     return fallback
 }
