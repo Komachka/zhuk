93b1696fce198ec8ac53478856c126ab5799e041
Kateryna Storozh <storozhkateryna@gmail.com>
2019-08-07 14:33:53 +0300

    [https://gitlab.com/evo.company/evo.zhuk-android/issues/2] crate constants

--- data/src/main/java/com/kstorozh/data/network/CalendarApi.kt
+++ data/src/main/java/com/kstorozh/data/network/CalendarApi.kt
@@ -1,6 +1,8 @@
 package com.kstorozh.data.network
 
+import END_DATE
 import GET_BOOKING_URL
+import START_DATE
 import com.kstorozh.data.models.BookingDataByDay
 import retrofit2.Response
 import retrofit2.http.GET
@@ -8,6 +10,6 @@ import retrofit2.http.Path
 
 internal interface CalendarApi {
     @GET(GET_BOOKING_URL)
-    suspend fun getBookingByDate(@Path("start_date")start_date: String, @Path("end_date")end_date: String):
+    suspend fun getBookingByDate(@Path(START_DATE)start_date: String, @Path(END_DATE)end_date: String):
             Response<BookingDataByDay>
 }
