a85d4d52010078c9c7fd904842461349a0750442
Kateryna Storozh <storozhkateryna@gmail.com>
2019-08-12 14:21:22 +0300

    implement jodo yime lib to parse data woth ISO 8601

--- domain/src/main/java/com/kstorozh/domain/GetBookingUseCaseImpl.kt
+++ domain/src/main/java/com/kstorozh/domain/GetBookingUseCaseImpl.kt
@@ -7,7 +7,9 @@ import com.kstorozh.domain.mapper.ErrorMapper
 import com.kstorozh.domainapi.GetBookingUseCase
 import com.kstorozh.domainapi.model.BookingInfo
 import com.kstorozh.domainapi.model.DomainResult
+import java.text.SimpleDateFormat
 
+const val DAY_MONTH_YEAR_FORMAT = "dd-MM-yyyy"
 class GetBookingUseCaseImpl(
     val bookingRepository: CalendarRepository,
     val errorMapper: ErrorMapper,
@@ -15,7 +17,10 @@ class GetBookingUseCaseImpl(
 ) : GetBookingUseCase {
 
     override suspend fun loadBooking(startDate: Long, endDate: Long): DomainResult<BookingInfo> {
-        val repoResult = bookingRepository.getBookingByDate(startDate.toString(), endDate.toString())
+        val dateFormat = SimpleDateFormat(DAY_MONTH_YEAR_FORMAT)
+
+        val repoResult = bookingRepository.getBookingByDate(
+            dateFormat.format(startDate), dateFormat.format(endDate))
         Log.d("MainActivity", "result ${repoResult.data?.dayData}")
         Log.d("MainActivity", "error ${repoResult.error}")
         val domainError = errorMapper.mapToDomainError(repoResult.error)
