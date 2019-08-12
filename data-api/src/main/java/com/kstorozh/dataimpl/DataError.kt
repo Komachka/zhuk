f9faabe2a23cf8fe1d91cba55f0866f308b63e94
Kateryna Storozh <storozhkateryna@gmail.com>
2019-08-12 10:53:57 +0300

    update models

--- data-api/src/main/java/com/kstorozh/dataimpl/DataError.kt
+++ data-api/src/main/java/com/kstorozh/dataimpl/DataError.kt
@@ -1,6 +1,6 @@
 package com.kstorozh.dataimpl
 
-class DataError(
+data class DataError(
     val errorStatus: ErrorStatus? = null,
     val errorMessage: String? = null,
     val throwable: Throwable
