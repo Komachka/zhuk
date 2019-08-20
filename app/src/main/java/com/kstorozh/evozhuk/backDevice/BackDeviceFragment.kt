c4d2aaf123795e9b295efaf2fbdc5a8451a30c38
Kateryna Storozh <storozhkateryna@gmail.com>
2019-08-20 15:33:41 +0300

    add padding to ini device

--- app/src/main/java/com/kstorozh/evozhuk/backDevice/BackDeviceFragment.kt
+++ app/src/main/java/com/kstorozh/evozhuk/backDevice/BackDeviceFragment.kt
@@ -13,6 +13,8 @@ import android.app.NotificationManager
 import android.content.Context
 import android.content.Intent
 import com.kstorozh.evozhuk.*
+import com.kstorozh.evozhuk.chooseTime.ChooseTimeFragmentArgs
+import com.kstorozh.evozhuk.chooseTime.ChooseTimeFragmentDirections
 import com.kstorozh.evozhuk.notifications.NotificationService
 import com.kstorozh.evozhuk.utils.getDeviceName
 import com.kstorozh.evozhuk.utils.observe
@@ -36,6 +38,13 @@ class BackDeviceFragment : Fragment(), HandleErrors {
         super.onViewCreated(view, savedInstanceState)
         view.infoImageBut.setOnClickListener {
             Navigation.findNavController(view).navigate(BackDeviceFragmentDirections.actionBackDeviceFragmentToInfoFragment())
+        }
+        view.calendarImageBut.setOnClickListener{
+            arguments?.let {
+                with(BackDeviceFragmentDirections.actionBackDeviceFragmentToCalendarFragment(BackDeviceFragmentArgs.fromBundle(it).userId)) {
+                    Navigation.findNavController(view).navigate(this)
+                }
+            }
         }
         view.deviceNameTv.text = context?.getDeviceName()
         view.youTakeDeviceLabelTv.text = "${resources.getString(R.string.youTableDeviceLabel)} ${context?.getDeviceName()}"
--- app/src/main/java/com/kstorozh/evozhuk/chooseTime/ChooseTimeFragment.kt
+++ app/src/main/java/com/kstorozh/evozhuk/chooseTime/ChooseTimeFragment.kt
@@ -22,6 +22,8 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kstorozh.evozhuk.*
 import com.kstorozh.evozhuk.backDevice.MyNotificationPublisher
+import com.kstorozh.evozhuk.calendar.BookOrTakeFragmentArgs
+import com.kstorozh.evozhuk.calendar.BookOrTakeFragmentDirections
 import com.kstorozh.evozhuk.notifications.CHANEL_ID
 import com.kstorozh.evozhuk.notifications.NotificationService
 import com.kstorozh.evozhuk.utils.getDeviceName
@@ -56,6 +58,13 @@ class ChooseTimeFragment : Fragment(), HandleErrors {
         val modelChooseTime = ViewModelProviders.of(activity!!).get(ChooseTimeViewModel::class.java)
         with(viewLifecycleOwner) {
             handleErrors(modelChooseTime, view)
+        }
+        view.calendarImageBut.setOnClickListener{
+            arguments?.let {
+                with(ChooseTimeFragmentDirections.actionChooseTimeFragmentToCalendarFragment(ChooseTimeFragmentArgs.fromBundle(it).userId)) {
+                    Navigation.findNavController(view).navigate(this)
+                }
+            }
         }
         val anotherTimeMilisec = ChooseTimeFragmentArgs.fromBundle(arguments!!).milisec
         val userId = ChooseTimeFragmentArgs.fromBundle(arguments!!).userId
