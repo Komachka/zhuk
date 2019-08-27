fac99c410e3510a59dc8ffad4d4b35681ae03b2f
Kateryna Storozh <storozhkateryna@gmail.com>
2019-08-27 17:03:49 +0300

    parsing data with joda time

--- domain/src/main/java/com/kstorozh/domain/mapper/DeviceInfoMapper.kt
+++ domain/src/main/java/com/kstorozh/domain/mapper/DeviceInfoMapper.kt
@@ -7,6 +7,8 @@ import com.kstorozh.dataimpl.model.out.BookingSessionData
 import com.kstorozh.domainapi.model.BookingInputData
 import com.kstorozh.domainapi.model.DeviceInputData
 import com.kstorozh.domainapi.model.SessionData
+import org.joda.time.DateTime
+import org.joda.time.format.ISODateTimeFormat
 import java.text.SimpleDateFormat
 
 import java.util.*
@@ -25,11 +27,13 @@ class DeviceInfoMapper {
 
     @SuppressLint("SimpleDateFormat")
     fun mapBookingParam(bookingInputData: BookingInputData, startDate: Calendar? = null): BookingParam {
-        val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'+03:00'")
+        val foramtter = ISODateTimeFormat.dateTime()
+        val start =  startDate?.let { foramtter.print(it.timeInMillis) } ?: DateTime().toString()
+        val end =  bookingInputData.endDate?.let { foramtter.print(it.timeInMillis) } ?: DateTime().toString()
         return BookingParam(
             bookingInputData.userId,
-            startDate?.let { format.format(startDate.time) } ?: "2019-07-07T00:00:00+03:00",
-            bookingInputData.endDate?.let { format.format(bookingInputData.endDate!!.time) } ?: "2019-07-07T00:00:00+03:00",
+            start,
+            end,
             bookingInputData.isForce
         )
     }
@@ -37,8 +41,8 @@ class DeviceInfoMapper {
     @SuppressLint("SimpleDateFormat")
     fun mapBookingSession(bookingSession: BookingSessionData): SessionData {
         val endDateCalendar = Calendar.getInstance()
-        val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'+03:00'")
-        endDateCalendar.setTime(format.parse(bookingSession.endDate)!!)
+        val foramatter = ISODateTimeFormat.dateTimeParser()
+        endDateCalendar.time = foramatter.parseDateTime(bookingSession.endDate).toDate()
         return SessionData(bookingSession.userId, endDateCalendar)
     }
 }
