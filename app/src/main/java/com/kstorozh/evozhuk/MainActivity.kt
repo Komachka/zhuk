3af3c21b6863397fad56fe2346189ccb855bfe82
Kateryna Storozh <storozhkateryna@gmail.com>
2019-08-30 09:28:40 +0300

    add exit, fix dialog on shake

--- app/src/main/java/com/kstorozh/evozhuk/MainActivity.kt
+++ app/src/main/java/com/kstorozh/evozhuk/MainActivity.kt
@@ -13,64 +13,24 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.widget.Toast
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.kstorozh.evozhuk.shake_detector.ShakeDetector
 
 import kotlin.math.sqrt
 
 
-class MainActivity : AppCompatActivity(), SensorEventListener {
+class MainActivity : AppCompatActivity(), ShakeDetector.OnShakeListener {
+    override fun onShake(count: Int) {
+        showDialog()
+        //Toast.makeText(applicationContext, "Hello", Toast.LENGTH_LONG).show()
+    }
 
 
     private lateinit var sensorManager: SensorManager
     private var shake: Sensor? = null
+    private var shakeDetector: ShakeDetector? = null
+    
+    
 
-
-    private val SHAKE_THRESHOLD_GRAVITY = 2.7f
-    private val SHAKE_SLOP_TIME_MS = 500
-    private val SHAKE_COUNT_RESET_TIME_MS = 3000
-
-
-    private var mShakeTimestamp: Long = 0
-    private var mShakeCount: Int = 0
-
-
-
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-        // ignore
-    }
-
-    override fun onSensorChanged(event: SensorEvent) {
-        val x = event.values[0]
-        val y = event.values[1]
-        val z = event.values[2]
-
-        val gX = x / SensorManager.GRAVITY_EARTH
-        val gY = y / SensorManager.GRAVITY_EARTH
-        val gZ = z / SensorManager.GRAVITY_EARTH
-
-        // gForce will be close to 1 when there is no movement.
-        val gForce = sqrt((gX * gX + gY * gY + gZ * gZ).toDouble())
-
-        if (gForce > SHAKE_THRESHOLD_GRAVITY) {
-            val now = System.currentTimeMillis()
-            // ignore shake events too close to each other (500ms)
-            if (mShakeTimestamp + SHAKE_SLOP_TIME_MS > now) {
-                return
-            }
-
-            // reset the shake count after 3 seconds of no shakes
-            if (mShakeTimestamp + SHAKE_COUNT_RESET_TIME_MS < now) {
-                mShakeCount = 0
-            }
-
-            mShakeTimestamp = now
-            mShakeCount++
-
-
-            showDialog()
-            //Toast.makeText(applicationContext, "mShakeCount $mShakeCount", Toast.LENGTH_LONG).show()
-            Log.d(LOG_TAG, "mShakeCount $mShakeCount")
-        }
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
@@ -79,26 +39,27 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         setContentView(R.layout.activity_main)
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         shake = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        shakeDetector = ShakeDetector(this)
     }
 
 
     override fun onResume() {
         super.onResume()
         shake?.also { light ->
-            sensorManager.registerListener(this, light, SensorManager.SENSOR_DELAY_NORMAL)
+            sensorManager.registerListener(shakeDetector, light, SensorManager.SENSOR_DELAY_NORMAL)
         }
     }
 
     override fun onPause() {
         super.onPause()
-        sensorManager.unregisterListener(this)
+        sensorManager.unregisterListener(shakeDetector)
     }
 
 
     fun showDialog()
     {
-        val mBottomSheetDialog = BottomSheetDialog(applicationContext)
-        val sheetView = LayoutInflater.from(applicationContext).inflate(R.layout.bug_ot_wish_bottom_sheet_dialog, null)
+        val mBottomSheetDialog = BottomSheetDialog(this@MainActivity)
+        val sheetView = LayoutInflater.from(this@MainActivity).inflate(R.layout.bug_ot_wish_bottom_sheet_dialog, null)
         mBottomSheetDialog.setContentView(sheetView)
         mBottomSheetDialog.show()
     }
